80aa1022c3ca54315b8bcd946bba232cf0439153
Jan Armbrust <jan.armbrust@googlemail.com>
2024-09-19 13:59:05 +0200

    Handle displaying of paywall, Move from toolbar to buttons

--- AppExhibit/Views/ContentView.swift
+++ AppExhibit/Views/ContentView.swift
@@ -18,126 +18,100 @@ struct ContentView: View {
   @State private var showFindByAppStoreLinkSheet = false
   @State private var showPhotoZoomableSheet = false
   @State var selectedAppStoreLinkQRCodeData: Data? = nil
-  @State var showPaywall: Bool = false
 
   var body: some View {
-    NavigationStack {
-      List {
-        if self.items.isEmpty {
-          ContentUnavailableView {
-            Label("Add your first app", systemImage: "app.fill")
-          } description: {
-            Text(
-              "Add all your precious apps you want to easily share with the people around you just by tapping the + button"
-            )
-          } actions: {
-            Button {
-              self.showFindByAppNameSheet.toggle()
-            } label: {
-              Image(systemName: "plus")
-            }
-          }
-
-        } else {
-          ForEach(self.items) { item in
-            NavigationLink(value: item) {
-              HStack {
-                if let appIconData = item.icon, let appIcon = UIImage(data: appIconData) {
-                  AppIconView(appIcon: appIcon, size: 64)
-                }
-                Text(item.name)
-                if let appStoreLinkQRCodeData = item.qrCode {
-                  Spacer()
-                  Image(systemName: "qrcode")
-                    .onTapGesture {
-                      self.selectedAppStoreLinkQRCodeData = appStoreLinkQRCodeData
-                    }
-                    .padding(.trailing)
-                }
-              }
-            }
-          }
-          .onDelete(perform: self.deleteItems)
-        }
-      }
-      .navigationTitle("App Exhibit")
-      .navigationDestination(for: AppItem.self) { item in
-        AppDetailView(item: item)
-      }
-      .toolbar {
-        ToolbarItem {
-          Menu {
-            Button {
-              if self.items.isEmpty || FreemiumKit.shared.hasPurchased {
+      NavigationStack {
+        List {
+          Section {
+            if self.items.isEmpty {
+              Button("Add by search", systemImage: "magnifyingglass") {
                 self.showFindByAppNameSheet.toggle()
-              } else {
-                self.showPaywall = true
               }
-            } label: {
-              if self.items.isEmpty || FreemiumKit.shared.hasPurchased {
-                Label("Add by search", systemImage: "magnifyingglass")
-              } else {
-                Label("Add by search", systemImage: "lock")
-              }
-            }
-            Button {
-              if self.items.isEmpty || FreemiumKit.shared.hasPurchased {
+              Button("Add by App Store link", systemImage: "link") {
                 self.showFindByAppStoreLinkSheet.toggle()
-              } else {
-                self.showPaywall = true
               }
-            } label: {
-              if self.items.isEmpty || FreemiumKit.shared.hasPurchased {
-                Label("Add by App Store link", systemImage: "link")
-              } else {
-                Label("Add by App Store link", systemImage: "lock")
-              }
-            }
-            Button {
-              if self.items.isEmpty || FreemiumKit.shared.hasPurchased {
+              Button("Add manually", systemImage: "plus") {
                 self.showCreateAppSheet.toggle()
-              } else {
-                self.showPaywall = true
               }
-            } label: {
-              if self.items.isEmpty || FreemiumKit.shared.hasPurchased {
-                Label("Add manually", systemImage: "plus")
-              } else {
-                Label("Add manually", systemImage: "lock")
+            } else {
+              PaidFeatureButton("Add by search", systemImage: "magnifyingglass") {
+                self.showFindByAppNameSheet.toggle()
+              }
+              PaidFeatureButton("Add by App Store link", systemImage: "link") {
+                self.showFindByAppStoreLinkSheet.toggle()
+              }
+              PaidFeatureButton("Add manually", systemImage: "plus") {
+                self.showCreateAppSheet.toggle()
               }
             }
-          } label: {
-            Label("Add App", systemImage: "plus")
+          }
+          if self.items.isEmpty {
+            ContentUnavailableView {
+              Label("Add your first app", systemImage: "app.fill")
+            } description: {
+              Text(
+                "Add all your precious apps you want to easily share with the people around you just by tapping the + button"
+              )
+            } actions: {
+              Button {
+                self.showFindByAppNameSheet.toggle()
+              } label: {
+                Image(systemName: "plus")
+              }
+            }
+
+          } else {
+            ForEach(self.items) { item in
+              NavigationLink(value: item) {
+                HStack {
+                  if let appIconData = item.icon, let appIcon = UIImage(data: appIconData) {
+                    AppIconView(appIcon: appIcon, size: 64)
+                  }
+                  Text(item.name)
+                  if let appStoreLinkQRCodeData = item.qrCode {
+                    Spacer()
+                    Image(systemName: "qrcode")
+                      .onTapGesture {
+                        self.selectedAppStoreLinkQRCodeData = appStoreLinkQRCodeData
+                      }
+                      .padding(.trailing)
+                  }
+                }
+              }
+            }
+            .onDelete(perform: self.deleteItems)
           }
         }
-      }
-      .paywall(isPresented: self.$showPaywall)
-      .sheet(isPresented: self.$showCreateAppSheet) {
-        AddAppView(viewModel: .constant(AddAppViewModel()), newAppItem: .constant(AppItem()))
-      }
-      .sheet(isPresented: self.$showFindByAppNameSheet) {
-        FindByAppNameView()
-      }
-      .sheet(isPresented: self.$showFindByAppStoreLinkSheet) {
-        FindByAppStoreLinkView()
-      }
-      .sheet(isPresented: self.$showPhotoZoomableSheet) {
-        if let selectedAppStoreLinkQRCodeData {
-          PhotoZoomableView(appStoreLinkQRCodeData: selectedAppStoreLinkQRCodeData)
-            .presentationBackground(.ultraThinMaterial)
-            .presentationCornerRadius(16)
+        .navigationTitle("App Exhibit")
+        .navigationDestination(for: AppItem.self) { item in
+          AppDetailView(item: item)
         }
-      }
-      .task(id: self.selectedAppStoreLinkQRCodeData) {
-        if self.selectedAppStoreLinkQRCodeData != nil {
-          self.showPhotoZoomableSheet.toggle()
+        .sheet(isPresented: self.$showCreateAppSheet) {
+          AddAppView(viewModel: .constant(AddAppViewModel()), newAppItem: .constant(AppItem()))
         }
-      }
-      .onChange(of: self.showPhotoZoomableSheet) { _, newValue in
-        if newValue == false {
-          self.selectedAppStoreLinkQRCodeData = nil
+        .sheet(isPresented: self.$showFindByAppNameSheet) {
+          FindByAppNameView()
         }
-      }
+        .sheet(isPresented: self.$showFindByAppStoreLinkSheet) {
+          FindByAppStoreLinkView()
+        }
+        .sheet(isPresented: self.$showPhotoZoomableSheet) {
+          if let selectedAppStoreLinkQRCodeData {
+            PhotoZoomableView(appStoreLinkQRCodeData: selectedAppStoreLinkQRCodeData)
+              .presentationBackground(.ultraThinMaterial)
+              .presentationCornerRadius(16)
+          }
+        }
+        .task(id: self.selectedAppStoreLinkQRCodeData) {
+          if self.selectedAppStoreLinkQRCodeData != nil {
+            self.showPhotoZoomableSheet.toggle()
+          }
+        }
+        .onChange(of: self.showPhotoZoomableSheet) { _, newValue in
+          if newValue == false {
+            self.selectedAppStoreLinkQRCodeData = nil
+          }
+        }
     }
   }
 
