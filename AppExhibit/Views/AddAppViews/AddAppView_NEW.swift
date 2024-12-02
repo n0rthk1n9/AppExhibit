ee95c612fa93829f8c3058cdbd69e32d57c6d759
Jan Armbrust <jan.armbrust@googlemail.com>
2024-12-02 16:46:13 +0100

    Add fetching of name

--- AppExhibit/Views/AddAppViews/AddAppView_NEW.swift
+++ AppExhibit/Views/AddAppViews/AddAppView_NEW.swift
@@ -8,8 +8,11 @@
 import SwiftUI
 
 struct AddAppView_NEW: View {
-  @State private var progressState: ProgressState = .successful
+  @State private var progressState: ProgressState = .notStarted
   @State var appItem: AppItem = AppItem()
+  @State private var appDetails: [ITunesAPIResult] = []
+  
+  private let iTunesAPIService: ITunesAPIServiceProtocol = ITunesAPIService()
   
   let appStoreLink: String
 
@@ -40,6 +43,7 @@ struct AddAppView_NEW: View {
               TextField("App Store or TestFlight Link", text: $appItem.appStoreLink)
             }
           }
+          .padding(.top)
           Button {
             // add app
           } label: {
@@ -52,11 +56,84 @@ struct AddAppView_NEW: View {
           }
           .buttonStyle(.borderedProminent)
           .padding()
+          .navigationTitle("Add App")
         }
     }
+    .task {
+      await fetchAppDetails()
+    }
+  }
+  
+  private func fetchAppDetails() async {
+    if let appID = extractAppID(from: appItem.appStoreLink) {
+      await getAppDetails(for: appID)
+      appItem.name = appDetails.first?.trackCensoredName ?? ""
+    }
+  }
+  
+  private func extractAppID(from urlString: String) -> String? {
+    progressState = .inProgress
+
+    guard !urlString.isEmpty else {
+      progressState = .failed(error: AppExhibitError.notAnAppStoreLink)
+      return nil
+    }
+
+    guard let url = URL(string: urlString) else {
+      progressState = .failed(error: AppExhibitError.notAnAppStoreLink)
+      return nil
+    }
+
+    let pathComponents = url.pathComponents
+    let regexPattern = #"id(\d+)"#
+
+    do {
+      let regex = try NSRegularExpression(pattern: regexPattern)
+      for component in pathComponents {
+        let range = NSRange(location: 0, length: component.utf16.count)
+        if let match = regex.firstMatch(in: component, options: [], range: range) {
+          if let idRange = Range(match.range(at: 1), in: component) {
+            return String(component[idRange])
+          }
+        }
+      }
+    } catch let error as AppExhibitError {
+      progressState = .failed(error: error)
+      return nil
+    } catch {
+      if (error as? URLError)?.code == .cancelled {
+        return nil
+      }
+      progressState = .failed(error: .other(error: error))
+      return nil
+    }
+
+    progressState = .successful
+
+    return nil
+  }
+  
+  func getAppDetails(for id: String) async {
+    self.progressState = .inProgress
+
+    do {
+      let fetchedAppDetails = try await iTunesAPIService.fetchAppDetails(for: id)
+
+      appDetails = fetchedAppDetails
+
+    } catch let error as AppExhibitError {
+      self.progressState = .failed(error: error)
+    } catch {
+      if (error as? URLError)?.code == .cancelled {
+        return
+      }
+      self.progressState = .failed(error: .other(error: error))
+    }
+
+    self.progressState = .successful
   }
 }
 
 #Preview {
-  AddAppView_NEW(appStoreLink: "")
+  AddAppView_NEW(appStoreLink: "https://apps.apple.com/de/app/pixelmator-pro/id1289583905?l=en-GB&mt=12")
 }
